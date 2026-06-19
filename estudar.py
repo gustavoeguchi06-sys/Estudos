d1867162dec26bbb020c8752ac35286aa994f9ca
Gustavo Eguchi <gustavoeguchi06@gmail.com>
2026-06-18 21:36:10 -0300

    Atualiza estudar.py

--- estudar.py
+++ estudar.py
@@ -1,3 +1,28 @@
-print("ola mundo")
+while True:
+    A = float(input("Digite o primeiro número: "))
+    operacao = input("Digite a operação (+, -, *, /): ")
+    B = float(input("Digite o segundo número: "))
 
-print ("amo minha mae e minha vó")
+    if operacao == "+":
+        print("Resultado:", A + B)
+
+    elif operacao == "-":
+        print("Resultado:", A - B)
+
+    elif operacao == "*":
+        print("Resultado:", A * B)
+
+    elif operacao == "/":
+        if B != 0:
+            print("Resultado:", A / B)
+        else:
+            print("Não é possível dividir por zero.")
+
+    else:
+        print("Operação inválida.")
+
+    continuar = input("\nDeseja fazer outro cálculo? (s/n): ")
+
+    if continuar.lower() != "s":
+        print("Calculadora encerrada!")
+        break
